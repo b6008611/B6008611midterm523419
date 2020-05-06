e930043ada664fbd8b297b1453811e68ef5a6d4e
b6008611 <patchareeyapromhaing@gmail.com>
2020-05-06 22:26:09 +0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'B6008611midterm523419';
+}
